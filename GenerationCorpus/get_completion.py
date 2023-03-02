76b193b7d62175c5fb527c30826a9b80e33578e7
Pedro Alves <pedromfa98@gmail.com>
2023-03-02 20:56:56 +0000

    question template formating done

--- GenerationCorpus/get_completion.py
+++ GenerationCorpus/get_completion.py
@@ -1,5 +1,30 @@
 import openai
 
+"""
+    format_text structure:
+        {
+            "client": name,
+            "solar_power_cat": low/moderate/high,
+            "solar_power_num": number,
+            "explanation": text,
+            "use_devices": text,
+            "uncertain_devices": text,
+            "nouse_devices": text
+        }
+
+"""
+
+def gen_question(format_text):
+    raw_question = ""
+    with open("./GenerationCorpus/resources/question_template.txt", "r") as f:
+        raw_question = f.read().split("#")
+
+    for i in range(1, len(raw_question), 2):
+        raw_question[i] = format_text[raw_question[i]]
+
+    return "".join(raw_question)
+
+
 def gen_completion(question):
     openai.api_key = "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"
     completion = openai.ChatCompletion.create(
@@ -9,12 +34,16 @@ def gen_completion(question):
 
     return completion
 
-question = """
-    I am trying to build small reports for my clients relative to generated solar power prediction from their solar panels.
-    Take for instance that, for my client Luís Faria the solar power generated will be moderate (1 kilo Watts) due to the forecast of high temperature (30 degrees celsius) and high solar irradiance (200 Watts per meter square).
-    He can use his washing machine and air conditioner, his dishwasher might exceed the generated power, and his water heater will exceed the generated power. 
-    Can you build me a small report for my client with this information?
-"""
+format_text = {
+            "client": "Pedro",
+            "solar_power_cat": "high",
+            "solar_power_num": "1",
+            "explanation": "moderate ambient temperature (20 degrees celsius)",
+            "use_devices": "washing machine",
+            "uncertain_devices": "dishwasher and air conditioner",
+            "nouse_devices": "none"
+        }
 
+question = gen_question(format_text)
 completion = gen_completion(question)
 print(completion)
