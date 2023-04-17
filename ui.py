d0c2389a08f732365491b219da55b160b3ee1a46
rafaeldpsilva <1190979@isep.ipp.pt>
2023-04-17 14:03:40 +0100

    ui

--- ui.py
+++ ui.py
@@ -6,16 +6,10 @@ from shapModel.shapModel import getContributions
 
 def fetch_forecast(url):
     return [0,0,0,0,0,0,50,100,350,800,1100,1500,2000,2300,2350,2123,1800,1304,754,300,213,0,0,0]
-    response = requests.get(url+'/forecast')
-    data = response.json()
-    return data['forecast']
 
-def fetch_report(url):
+def fetch_report(prediction, all_contribution, mean_contribution,listDevices):
     time.sleep(5)
     return 'dear carlos , we hope this message finds you well . as your solar power provider , we wanted to provide you with a report on the predicted solar power generation from your solar panels on october 15th , 2024 at 1800 . based on our analysis , we predict that the solar power generated will be medium , with a total of 5 . 0 kilo watts . this is due to the negative impact of the solar radiation and hourly precipitation predicted value . based on this prediction , we recommend that you avoid using your appliances heater , as it might exceed the generated power . however , we advise against using your washing machine and dishwasher as they will definitely exceed the generated power . we hope this information is helpful and please do not hesitate to contact us if you have any further questions or concerns . best regards , tenergito'
-    response = requests.get(url+'/report')
-    data = response.json()
-    return data['report']
 
 
 sl.set_page_config(page_title="Tenergito",layout="wide")
@@ -34,9 +28,10 @@ forecast = fetch_forecast(url)
 forecast = pd.DataFrame(forecast, columns = ['generation'])
 left.line_chart(forecast)
 
+name = right.text_input("Name:")
 
 devices = ["AC", "DishWasher", "WashingMachine", "Dryer", "WaterHeater" , "TV", "Microwave", "Kettle", "Lighting", "Refrigerator"]
-used_devices = right.multiselect('Devices to use:',devices)
+used_devices = right.multiselect('Devices to use (first has the most priority and the last the least priority):',devices)
 
 ordered_devices = []
 for i, device in enumerate(used_devices):
@@ -44,11 +39,12 @@ for i, device in enumerate(used_devices):
 
 datetime = right.date_input('What time?')
 hour = right.time_input('To what hour do you want advice?')
-sl.write()
-getContributions(ordered_devices, str(hour)[:-3])
 
+if right.button("Generate Report"):
+    prediction, all_contribution, mean_contribution,listDevices = getContributions(ordered_devices, str(hour)[:-3])
+    sl.write(prediction, all_contribution, mean_contribution,listDevices)
 
 sl.write('### report')
 with sl.spinner(text='Generating report..'):
-    sl.write(fetch_report(hour))
+    sl.write(fetch_report(prediction, all_contribution, mean_contribution,listDevices))
 
