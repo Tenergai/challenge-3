2758482d67afb626752d1e4f7c4aefe3ad796bb3
rafaeldpsilva <1190979@isep.ipp.pt>
2023-04-18 14:10:55 +0100

    serialize

--- ui.py
+++ ui.py
@@ -2,7 +2,8 @@ import streamlit as sl
 import requests
 import pandas as pd
 import time
-from shapModel.shapModel import getContributions
+import random
+#from shapModel.shapModel import getContributions
 
 def fetch_forecast(url):
     return [0,0,0,0,0,0,50,100,350,800,1100,1500,2000,2300,2350,2123,1800,1304,754,300,213,0,0,0]
@@ -41,10 +42,22 @@ datetime = right.date_input('What time?')
 hour = right.time_input('To what hour do you want advice?')
 
 if right.button("Generate Report"):
-    prediction, all_contribution, mean_contribution,listDevices = getContributions(ordered_devices, str(hour)[:-3])
-    sl.write(prediction, all_contribution, mean_contribution,listDevices)
+    #prediction, all_contribution, mean_contribution,listDevices = getContributions(ordered_devices, str(hour)[:-3])
+    #sl.write(prediction, all_contribution, mean_contribution,listDevices)
 
-sl.write('### report')
-with sl.spinner(text='Generating report..'):
-    sl.write(fetch_report(prediction, all_contribution, mean_contribution,listDevices))
+    sl.write('### report')
+    with sl.spinner(text='Generating report..'):
+        time.sleep(5)
+    text_element = sl.empty()
+    report = 'dear carlos , we hope this message finds you well . as your solar power provider , we wanted to provide you with a report on the predicted solar power generation from your solar panels on october 15th , 2024 at 1800 . based on our analysis , we predict that the solar power generated will be medium , with a total of 5 . 0 kilo watts . this is due to the negative impact of the solar radiation and hourly precipitation predicted value . based on this prediction , we recommend that you avoid using your appliances heater , as it might exceed the generated power . however , we advise against using your washing machine and dishwasher as they will definitely exceed the generated power . we hope this information is helpful and please do not hesitate to contact us if you have any further questions or concerns . best regards , tenergito'
+    wr = ''
+    for char in report:
+        wr = wr + char
+        time.sleep(random.randrange(0,10)/100)
+        text_element.write(wr)
+
+        
+else:
+    sl.write('### report')
+    sl.write('no report to be generated')
 
